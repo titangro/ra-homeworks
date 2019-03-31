015499c93074d5ceec957abef67f19730d14e60a
Roman <titangro@users.noreply.github.com>
2019-03-31 12:51:35 +0300

    Добавил refs в задание feedback

--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -1,20 +1,29 @@
 'use strict';
 
 function FeedbackForm({data, onSubmit}) {
+    let _name, _salutationForm, _email, _message, _snacksForm;
     return (
         <form className="content__form contact-form" onSubmit={(e) => {
             e.preventDefault();
-            const form = new FormData(e.nativeEvent.currentTarget);
+            //const form = new FormData(e.nativeEvent.currentTarget);
             let result = {};
-            for (const [k, v] of form) {
+            result.salutation = _salutationForm.querySelector('input[type="radio"]:checked').value
+            result.name = _name.value;
+            result.email = _email.value;
+            result.message = _message.value;
+            result.snacks = [];
+            for (const radio of _snacksForm.querySelectorAll('input[type="checkbox"]:checked')) {
+                result.snacks[result.snacks.length] = radio.value;
+            }         
+            /*for (const [k, v] of form) {
                 result[k] = v;
-            }            
+            }*/          
             onSubmit(JSON.stringify(result));
         }} >
             <div className="testing">
                 <p>Чем мы можем помочь?</p>
             </div>
-            <div className="contact-form__input-group" > 
+            <div className="contact-form__input-group" ref={elem => _salutationForm = elem} > 
                 <input className="contact-form__input contact-form__input--radio" defaultChecked={"Мистер" === data.salutation} id="salutation-mr" name="salutation" type="radio" value="Мистер"/>
                 <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
                 <input className="contact-form__input contact-form__input--radio" defaultChecked={"Мисис" === data.salutation} id="salutation-mrs" name="salutation" type="radio" value="Мисис"/>
@@ -24,11 +33,11 @@ function FeedbackForm({data, onSubmit}) {
             </div>
             <div className="contact-form__input-group">
                 <label className="contact-form__label" htmlFor="name">Имя</label>
-                <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text" value={data.name} />
+                <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text" onChange={() => console.log(_name.value)} defaultValue={data.name} ref={elem => _name = elem} />
             </div>
             <div className="contact-form__input-group">
                 <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
-                <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" value={data.email} />
+                <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" defaultValue={data.email} ref={elem => _email = elem} />
             </div>
             <div className="contact-form__input-group">
                 <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
@@ -39,9 +48,9 @@ function FeedbackForm({data, onSubmit}) {
             </div>
             <div className="contact-form__input-group">
                 <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
-                <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65" value={data.message}></textarea>
+                <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65" defaultValue={data.message} ref={elem => _message = elem}></textarea>
             </div>
-            <div className="contact-form__input-group">
+            <div className="contact-form__input-group" ref={elem => _snacksForm = elem}>
                 <p className="contact-form__label--checkbox-group">Хочу получить:</p>
                 <input className="contact-form__input contact-form__input--checkbox" defaultChecked={data.snacks.indexOf("пицца") !== -1} id="snacks-pizza" name="snacks" type="checkbox" value="пицца"/>
                 <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
