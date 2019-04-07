a71305ff729b5f492c8742725024a43919d1adc3
Roman <titangro@users.noreply.github.com>
2019-04-07 21:06:36 +0300

    Поправил

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,10 +1,11 @@
 const SearchBox = ({ value, filterBooks }) => {
     let search;
-    const handleChange = event => {
+    const handleChange = () => {
         filterBooks(search.value);
     }
 
     return (
-        <input type="text" placeholder="Поиск по названию или автору" ref={elem => search = elem} defaultValue={value} onChange={handleChange} />
+        <input type="text" placeholder="Поиск по названию или автору" ref={elem => search = elem} 
+            defaultValue={value} onChange={handleChange} />
     );
 };
