1437ad1592469c4811b3c45d816037189ee9eff3
Roman <titangro@users.noreply.github.com>
2019-04-07 19:00:20 +0300

    Выполнено задание

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,10 @@
-const SearchBox = () => {
+const SearchBox = ({ value, filterBooks }) => {
+    let search;
+    const handleChange = event => {
+        filterBooks(search.value);
+    }
+
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+        <input type="text" placeholder="Поиск по названию или автору" ref={elem => search = elem} defaultValue={value} onChange={handleChange} />
     );
 };
