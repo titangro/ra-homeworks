1bb0021e2db542030c118a1d15e89b3b8150e767
Roman <titangro@users.noreply.github.com>
2019-04-07 19:01:29 +0300

    Дополнено задание
    Убрано querySelector и classList. Переписал с использованием состояния

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,25 +1,31 @@
-const SubscribeForm = () => {
-    let form;
-    const handleSubmit = event => {
+class SubscribeForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.data = props.data;        
+        this.form
+    }
+
+    handleSubmit(event) {
         event.preventDefault();
     }
-    const handleValid = () => {
-        if (form.querySelector('input[type="email"]').validity.valid) {
-            form.classList.remove('is-error');
-            form.classList.add('is-valid');
+    handleValid(event) {
+        if (event.target.validity.valid) {
+            this.setState({valid: true})
         } else {
-            form.classList.add('is-error');
-            form.classList.remove('is-valid');
+            this.setState({valid: false})
         }
     }
 
-    return (
+    render () {
+        const cls = !this.state ? "form form--subscribe" :
+         this.state.valid ? "form form--subscribe is-valid" : ("form form--subscribe is-error");
+        return (
         <div className="subscribe__form">
-            <form className="form form--subscribe" onSubmit={handleSubmit} ref={element => form = element}>
+            <form className={cls} onSubmit={this.handleSubmit.bind(this)} ref={element => this.form = element}>
                 <h4 className="form-title">Подписаться:</h4>
                 <div className="form-group">
                 <label htmlFor="input-email" className="sr-only">Email</label>
-                <input onChange={handleValid} onClick={handleValid} type="email" id="input-email" placeholder="Email" className="form-control"/>
+                <input onChange={this.handleValid.bind(this)} type="email" id="input-email" placeholder="Email" className="form-control"/>
                 <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
                 <button type="submit" className="form-next">
                     <i className="material-icons">keyboard_arrow_right</i>
@@ -27,5 +33,6 @@ const SubscribeForm = () => {
                 </div>
             </form>
         </div>
-    )
+        )
+    }
 };
