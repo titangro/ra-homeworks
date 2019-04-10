41f7c60f81eca24e8828ca3faece7b2ffa1e4643
Roman <titangro@users.noreply.github.com>
2019-04-10 22:32:12 +0300

    Задания выполнены/исправлены

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,29 @@
 const FontSelector = ({fonts, selectedFont, onSelect}) => {
     return (
-        <div className="font-picker">
-            Выберите шрифт
+        <div style={{height:"250px",overflow:"scroll"}}>
+            {fonts.map((font, fontIndex) =>         
+                <div className="grid center font-item" key={fontIndex}>
+                    <input type="radio" name="font" defaultValue={selectedFont} value={font.name} id={font.name} onChange={() => onSelect(font)} />
+                    <label htmlFor={font.name} className={"grid-1"} key={fontIndex} >
+                        <div className="picture-font grid">
+                            {["a","b","c"].map((char, charIndex) => 
+                                <Char key={charIndex} font={font} char={char} />
+                                    )}                            
+                            </div>
+                    </label>                        
+                </div>
+            )}
         </div>
     )
 };
+
+
+const Char = ({font,char}) =>
+{
+    let src = `${font.path}/${char}.jpg`;
+    return (
+        <div className="char">
+            <img className="font-img" src={src} alt={char} />
+        </div>
+    )
+}
--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,13 @@
 const TextRenderLine = ({value, onChange}) => {
+	function handleChange(event) {
+		event.target.value = event.target.value.replace(/[^A-Za-z ]+/g, '');
+		onChange(event.target.value);
+		console.log(value)
+	}
+	
 	return (
-		<div className="type-text">
-			Наберите текст
+		<div class="type-text">
+			<textarea name="text" onChange={handleChange} id="font-text" cols="30" rows="2" placeholder="Введите текст для футболки"></textarea>			
 		</div>
 	);
 };
