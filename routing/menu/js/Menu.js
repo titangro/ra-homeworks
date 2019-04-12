6898e7ebad0c7aa72898e253574a348af8928d2f
Roman <titangro@users.noreply.github.com>
2019-04-12 19:40:32 +0300

    Выполнено задание

---
+++ routing/menu/js/Menu.js
@@ -0,0 +1,10 @@
+const Menu = props => {
+    return (
+        <nav className="menu">
+            <NavLink className="menu__item" exact activeClassName="menu__item-active" to="/">Главная</NavLink>
+            <NavLink className="menu__item" exact activeClassName="menu__item-active" to="/drift">Дрифт-такси</NavLink>
+            <NavLink className="menu__item" exact activeClassName="menu__item-active" to="/timeattack">Time Attack</NavLink>
+            <NavLink className="menu__item" exact activeClassName="menu__item-active" to="/forza">Forza Karting</NavLink>
+        </nav>
+    )
+}
