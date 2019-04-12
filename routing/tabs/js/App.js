12a9cf1e025481b7c1303412adc5e8c8afa12ffd
Roman <titangro@users.noreply.github.com>
2019-04-12 19:47:47 +0300

    Задание выполнено

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,18 @@
-
+const App = props => {
+    return (
+        <Router>
+            <div className="tabs">
+                <nav className="tabs__items">
+                    <NavLink className="tabs__item" exact activeClassName="tabs__item-active" to="/">Главная</NavLink>
+                    <NavLink className="tabs__item" exact activeClassName="tabs__item-active" to="/creator">Криэйтор</NavLink>
+                    <NavLink className="tabs__item" exact activeClassName="tabs__item-active" to="/fortune">Гадалка</NavLink>
+                </nav>
+                <div className="tabs__content">
+                    <Route path="/creator" exact component={Creator} />
+                    <Route path="/fortune" component={Fortune} />
+                    <Route path="/" component={Essay} />
+                </div>
+            </div>
+        </Router>
+    )
+}
